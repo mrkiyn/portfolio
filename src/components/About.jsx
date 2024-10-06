ae4e200bd33d78e49d4d460108a48bf7d75ff4df
iyanayi <mimamado@tesda.gov.ph>
2024-10-06 19:44:24 +0800

    fix: Fix responsiveness

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,12 +7,12 @@ function About({ aboutRef, skillsRef }) {
     <div className='' id="about" ref={aboutRef}>
         <div className="lg:px-20 xl:px-32 2xl:px-64 px-7 py-5 lg:py-24">
             <div className=''>
-                <span className='bg-gradient-to-r from-castleton to-emerald text-transparent bg-clip-text text-5xl font-black'>ABOUT ME</span>
+                <span className='bg-gradient-to-r from-castleton to-emerald text-transparent bg-clip-text text-2xl lg:text-5xl font-black'>ABOUT ME</span>
             </div>
 
-            <div className='py-10'>
-                <p className='text-xl'>
-                    Greetings! I am <b className='text-emerald'>Mark Ian Amado</b>, a dedicated <b className='text-emerald'>web developer</b> with expertise in HTML, CSS, JavaScript, React, Tailwind, Material-UI, Ruby, Ruby on Rails, PHP and Laravel.
+            <div className='py-5 lg:py-10'>
+                <p className='text-sm lg:text-xl'>
+                    Greetings! I am <b className='text-emerald'>Mark Ian Amado</b>, a dedicated <b className='text-emerald'>Full Stack Software Engineer</b> with expertise in HTML, CSS, JavaScript, React, Tailwind, Material-UI, Ruby, Ruby on Rails, PHP and Laravel.
                     I specialize in developing dynamic and responsive web applications, ensuring high-quality user experiences. My proficiency encompasses both front-end and back-end development,
                     enabling me to create elegant interfaces and robust systems. Committed to continuous professional development and user-centric design principles, I strive to deliver web solutions
                     that are both functional and aesthetically pleasing.
--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -33,10 +33,10 @@ function Education() {
   return (
     <div className=''>
       <div className='pb-10'>
-        <span className='bg-gradient-to-r from-castleton to-emerald text-transparent bg-clip-text text-3xl font-black'>EDUCATION</span>
+        <span className='bg-gradient-to-r from-castleton to-emerald text-transparent bg-clip-text text-2xl lg:text-3xl font-black'>EDUCATION</span>
       </div>
 
-      <div className="w-full h-fit px-24" ref={educationRef}>
+      <div className="w-full h-fit lg:px-24" ref={educationRef}>
         <div className="relative wrap overflow-hidden h-full">
           <div className="border-2 absolute border-emerald h-full left-1/2 transform -translate-x-1/2"></div>
 
@@ -44,29 +44,28 @@ function Education() {
           <div className="mb-8 flex justify-between items-center w-full fade-in">
             <div className="order-1 w-5/12"></div>
             <div className="order-1 w-5/12 px-1 py-4 text-right">
-              <p className="text-sm md:text-base leading-snug text-castleton font-semibold">August 2023 - June 2024</p>
-              <h4 className="font-bold text-lg bg-gradient-to-r from-castleton to-emerald text-transparent bg-clip-text md:text-2xl">Full Stack Web Development</h4>
-              <p className="text-base text-gray-500">Avion School</p>
+              <p className="text-md md:text-base leading-snug text-castleton font-semibold">August 2023 - June 2024</p>
+              <h4 className="font-bold bg-gradient-to-r from-castleton to-emerald text-transparent bg-clip-text text-sm md:text-2xl">Full Stack Web Development</h4>
+              <p className="text-sm lg:text-base text-gray-500">Avion School</p>
             </div>
           </div>
 
           {/* Timeline item 2 */}
           <div className="mb-8 flex justify-between items-center w-full fade-in1">
             <div className="order-1 w-5/12 px-1 py-4 text-left">
-              <p className="text-sm md:text-base leading-snug text-castleton font-semibold">June 2018 - July 2023</p>
-              <h4 className="font-bold text-lg bg-gradient-to-r from-castleton to-emerald text-transparent bg-clip-text md:text-2xl">Bachelor of Science in Information Technology</h4>
-              <p className="text-base text-gray-500">Our Lady of Fatima University</p>
+              <p className="text-md md:text-base leading-snug text-castleton font-semibold">June 2018 - July 2023</p>
+              <h4 className="font-bold bg-gradient-to-r from-castleton to-emerald text-transparent bg-clip-text text-sm md:text-2xl">Bachelor of Science in Information Technology</h4>
+              <p className="text-sm text-gray-500">Our Lady of Fatima University</p>
             </div>
             <div className="order-1 w-5/12"></div>
           </div>
 
-          {/* Timeline item 3 */}
-          <div className="flex justify-between items-center w-full fade-in2">
+          <div className="mb-8 flex justify-between items-center w-full fade-in">
             <div className="order-1 w-5/12"></div>
             <div className="order-1 w-5/12 px-1 py-4 text-right">
-              <p className="text-sm md:text-base leading-snug text-castleton font-semibold">June 2016 - March 2018</p>
-              <h4 className="font-bold text-lg bg-gradient-to-r from-castleton to-emerald text-transparent bg-clip-text md:text-2xl">IT in Mobile App and Web Development</h4>
-              <p className="text-base text-gray-500">STI College (Senior High School)</p>
+              <p className="text-md md:text-base leading-snug text-castleton font-semibold">June 2016 - March 2018</p>
+              <h4 className="font-bold bg-gradient-to-r from-castleton to-emerald text-transparent bg-clip-text text-sm md:text-2xl">IT in Mobile App and Web Development</h4>
+              <p className="text-sm lg:text-base text-gray-500">STI College (Senior High School)</p>
             </div>
           </div>
         </div>
